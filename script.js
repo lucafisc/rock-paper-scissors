fd999cdd2e6e4342b79d7cac59507531f39a4d0e
Luca Fischer <lucafischer@fastmail.com>
2022-05-09 11:51:40 +0200

    Add computer random selection and game round functions

--- script.js
+++ script.js
@@ -1,19 +1,64 @@
-console.log("Hello");
+let computerSelection;
+let playerSelection = "rock";
 
-function computerPlay () {
-    let computerRandomNumber = Math.floor(Math.random()*3);
+function computerPlay() {
+  computerRandomNumber = Math.floor(Math.random() * 3);
 
-    switch (computerRandomNumber){
-    case 0 :
-        console.log("Computer chooses Rock!");
-        break;
-    case 1 :
-        console.log("Computer chooses Paper!");
-         break;
-     case 2 :
-        console.log("Computer chooses Scissors!");
-        break;
-    }
+  switch (computerRandomNumber) {
+    case 0:
+      computerSelection = "rock";
+      break;
+    case 1:
+      computerSelection = "paper";
+      break;
+    case 2:
+      computerSelection = "scissors";
+      break;
+  }
+  console.log(`Computer chooses ${computerSelection}`);
+  return computerSelection;
 }
-computerPlay();
 
+
+function playRound() {
+    computerSelection = computerPlay();
+      playerSelection.toLowerCase();
+
+  let roundMessage;
+  if (playerSelection == computerSelection) {
+    roundMessage = "It's a tie!";
+  } else if (playerSelection !== computerSelection) {
+    if (computerSelection == "rock") {
+      switch (playerSelection) {
+        case "paper":
+          roundMessage = `${playerSelection} beats ${computerSelection}. You won!`;
+          break;
+        case "scissors":
+          roundMessage = `${computerSelection} beats ${playerSelection}. You lost!`;
+          break;
+      }
+    } else if (computerSelection == "paper") {
+      switch (playerSelection) {
+        case "scissors":
+          roundMessage = `${playerSelection} beats ${computerSelection} You won!`;
+          break;
+        case "rock":
+          roundMessage = `${computerSelection} beats ${playerSelection} You lost!`;
+          break;
+      }
+    } else if (computerSelection == "scissors") {
+      switch (playerSelection) {
+        case "rock":
+          roundMessage = `${playerSelection} beats ${computerSelection} You won!`;
+          break;
+        case "paper":
+          roundMessage = `${computerSelection} beats ${playerSelection} You lost!`;
+          break;
+      }
+    }
+  }
+  console.log(roundMessage);
+  
+}
+ 
+playRound();
